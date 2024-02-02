1a026a79fe7e1cfa01851ec72f86fbd3fcdfd38e
Sani-aliyu <88440258+Sani-aliyu@users.noreply.github.com>
2024-02-02 21:59:36 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,55 +1,57 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _printf - custom printf function
- * @format: format string
- * Return: the number of characters printed (excluding the null byte)
+ * _printf - Custom printf function with limited functionality
+ * @format: Format string with conversion specifiers
+ * @...: Additional arguments based on the conversion specifiers
+ *
+ * Return: Number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
+    char c;
 
     va_start(args, format);
 
     while (*format)
     {
-        if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%'))
+        if (*format == '%')
         {
-            switch (*(format + 1))
+            format++;
+            switch (*format)
             {
-            case 'c':
-                count += _putchar(va_arg(args, int));
-                break;
-            case 's':
-            {
-                char *str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    count += _putchar(*str);
-                    str++;
-                }
-                break;
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    count += write(1, va_arg(args, char *), 0);
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    count++;
+                    break;
+                default:
+                    write(1, "%", 1);
+                    write(1, format, 1);
+                    count += 2;
+                    break;
             }
-            case '%':
-                count += _putchar('%');
-                break;
-            default:
-                break;
-            }
-            format += 2; /* Move to the next character after the format specifier */
         }
         else
         {
-            count += _putchar(*format);
-            format++;
+            write(1, format, 1);
+            count++;
         }
+
+        format++;
     }
 
     va_end(args);
 
     return count;
 }
-
